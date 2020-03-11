19de430172ccb4a396719acfe0e2adcb18d5a677
XBB1995 <cs358118276@qq.com>
2020-03-11 21:01:11 +0800

    first project

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,25 @@ package com.company;
  * @author Administrator
  */
 public class Main {
+  public static void main(String[] args) {
+    System.out.println(getData(1));
+//    单引号中必须有且只有一个字符 可以为空格
+//    System.out.print('');
+  }
 
-    public static void main(String[] args) {
-        // write your code here
-        System.out.println("hello world!");
-    }
+  public static double getData(int number) {
+//    int number1 = 123;
+//    double number2 = 3.14;
+//    char c = 'a';
+//    boolean isShow = true;
+//    long numberLong = 300000000000000000L;
+//    float numberFloat = 2.14F;
+//    int a = 10, b = 20, cc = 30;
+    int num1 = (int) 6000000000L;
+//    byte 表示的数据范围是 -128 ~ 127
+//    int num4 = 40, num5 = 50;
+//    byte result1 = (byte)(num4 + num5);
+    String str = "hello";
+    return  10/3.99;
+  }
 }
